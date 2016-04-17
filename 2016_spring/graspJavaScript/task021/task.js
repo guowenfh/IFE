985d2a6f4ae25e0b49c2827974667e58f206fe10
guowenfh <guowen921@live.com>
2016-04-17 14:06:03 +0800

    fix

--- 2016_spring/graspJavaScript/task021/task.js
+++ 2016_spring/graspJavaScript/task021/task.js
@@ -1,6 +1,6 @@
 (function() {
     var arr = [];
-
+    //tag输入框
     function tagInpFun(value, obj) {
         if (value !== "" && arr.indexOf(value) === -1) {
             if (arr.length < 10) {
@@ -13,7 +13,7 @@
         arrToHtml(obj, arr);
     }
     var interest = [];
-
+    //处理兴趣列表
     function interestInpFun(value, obj) {
         //将输入分割成数组，并且进行去重操作
         var vArr = uniqArray(value.split(/\n|\s+|\,|\，|\、|\;|\；/));
@@ -27,17 +27,16 @@
             }
         }
         arrToHtml(obj, interest);
-
     }
     /**
      * html渲染函数
      * @param  {HTMLElement} obj 用于渲染的容器
-     * @param  {Arrary} arr 用于设置的内容
+     * @param  {Array} arr 用于设置的内容
      */
     function arrToHtml(obj, arr) {
         obj.innerHTML = "";
         if (arr.length !== 0) {
-            arr.forEach(function(ele, index, arr) {
+            arr.forEach(function(ele, index) {
                 if (index < 10) {
                     obj.innerHTML += "<li>" + ele + "</li>";
                 }
@@ -47,21 +46,19 @@
     /**
      * 设置hover效果，删除数组项并且重新渲染html
      * @param  {HTMLElement} obj [用来事件代理的html元素]
-     * @param  {Arrary} arr [用于渲染html的数组]
+     * @param  {Array} arr [用于渲染html的数组]
      */
-    function romoveList(obj, arr) {
+    function removeList(obj, arr) {
         addEvent(obj, "mouseover", function(ev) {
-            var target = null;
             if (ev.target.nodeName.toLowerCase() === "li") {
-                target = ev.target;
+               var target = ev.target;
                 target.innerHTML = "点击删除" + target.innerText;
                 target.style.backgroundColor = "#DF4210";
             }
         });
         addEvent(obj, "mouseout", function(ev) {
-            var target = null;
             if (ev.target.nodeName.toLowerCase() === "li") {
-                target = ev.target;
+                var target = ev.target;
                 var asd = target.innerText;
                 target.innerHTML = "";
                 target.innerHTML = asd.substr(4);
@@ -71,7 +68,7 @@
         if (arr) {
             addEvent(obj, "mousedown", function(ev) {
                 if (ev.target.nodeName.toLowerCase() === "li") {
-                    target = ev.target;
+                   var target = ev.target;
                     arr.splice(arr.indexOf(target.innerText.substr(4)), 1);
                     target.innerHTML = "";
                     arrToHtml(this, arr);
@@ -94,10 +91,9 @@
                 this.value = "";
             }
         });
-        romoveList(tagList, arr);
+        removeList(tagList, arr);
         addEvent(interestBtn, "click", function() {
             interestInpFun(trim(interestInp.value), interestList);
-            romoveList(interestList);
         });
     }
     init();
