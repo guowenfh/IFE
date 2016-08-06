efa5772184c3f1fa6a44d2653adc48d47899ccbf
guowenfh <guowen921@live.com>
2016-08-07 00:41:24 +0800

    完成eslint配置，随时调整！

--- 2016_spring/graspJavaScript/task026/task.js
+++ 2016_spring/graspJavaScript/task026/task.js
@@ -1,17 +1,18 @@
-(function(window, document) {
+(function (window, document) {
+    
     /**
      * 表单验证函数
      * @param  {String} text 待验证的输入
      * @return {String}      错误码
      */
-    function inputCheck(text) {
+    function inputCheck (text) {
         var text = trim(text);
-        var result = "";
+        var result = '';
         var cont = 0;
-        if (text === "") {
-            result = "名字不能为空！";
+        if (text === '') {
+            result = '名字不能为空！';
         } else {
-            text.split("").forEach(function(item) {
+            text.split('').forEach(function (item) {
                 if (/[^\x00-\xff]/.test(item)) {
                     cont += 2;
                 } else {
@@ -19,32 +20,32 @@
                 }
             });
             if (cont >= 4 && cont <= 16) {
-                result = "名称格式正确";
+                result = '名称格式正确';
             } else {
-                result = "长度为4～16个字符";
+                result = '长度为4～16个字符';
             }
         }
         return result;
     }
 
-    function changeClass(eleArr, info) {
-        if (info && info === "名称格式正确") {
-            eleArr.forEach(function(item) {
-                item.className = "success";
+    function changeClass (eleArr, info) {
+        if (info && info === '名称格式正确') {
+            eleArr.forEach(function (item) {
+                item.className = 'success';
             });
         } else {
-            eleArr.forEach(function(item) {
-                item.className = "error";
+            eleArr.forEach(function (item) {
+                item.className = 'error';
             });
         }
 
     }
 
-    function init() {
+    function init () {
         var input = document.querySelector('#inp');
         var button = document.querySelector('#button');
-        var tipInfo = document.querySelector("#name-tips");
-        addEvent(button, "click", function() {
+        var tipInfo = document.querySelector('#name-tips');
+        addEvent(button, 'click', function () {
             var result = inputCheck(input.value);
             tipInfo.innerHTML = result;
             changeClass([tipInfo, input], result);
