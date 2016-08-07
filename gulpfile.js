c9c48078a0b5098c5d5b855dbc2dcd426fc59434
guowenfh <chenguowen@raycloud.com>
2016-08-07 22:15:05 +0800

    完成gulp浏览器自动刷新配置

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,53 @@
 var gulp = require('gulp');
-var eslint = require('gulp-eslint');
+// 这个插件用来管理所有在package.json里面的gulp插件！用再辛苦一个一个加载了
+// 连接符将自动转化成驼峰
+var gulpLoadPlugins = require('gulp-load-plugins');
+var plugins = gulpLoadPlugins();
+// 浏览器自动刷新插件，因为不是gulp模块所以需要单独声明
+var browserSync = require('browser-sync');
 
-var url = '2016_spring/graspJavaScript/task026/task.js';
-gulp.task('default', function() {
-    // 代码块放在这里
-    return gulp.src(url)
-        .pipe(eslint())
-        .pipe(gulp.dest('2016_spring/graspJavaScript/task026/build'));
+var base = './2016_spring/graspJavaScript/task026/';
+var urlConfig = 'task.js';
+
+// gulp.src,可以模糊匹配
+/**
+ * 使用eslint进行语法检测
+ */
+gulp.task('lint', function() {
+    return gulp.src(base + urlConfig)
+        .pipe(plugins.eslint())
+        .pipe(plugins.eslint.format())
+        .pipe(plugins.eslint.failAfterError());
 });
+
+/**
+ * 文件压缩
+ */
+gulp.task('minify', function() {
+    gulp.src(base + urlConfig)
+        .pipe(plugins.uglify())
+        .pipe(gulp.dest(base + 'build'));
+});
+
+var browser = './2016_spring/graspJavaScript/';
+
+/**
+ * 浏览器自刷新
+ */
+gulp.task('browser-sync', function() {
+    var files = [
+        browser + '**/*.html',
+        browser + '**/*.css',
+        browser + '**/*.js',
+    ];
+
+    browserSync.init(files, {
+        server: {
+            baseDir: browser,
+        },
+    });
+});
+gulp.task('default', ['browser-sync'], function() {
+    // content
+});
+
