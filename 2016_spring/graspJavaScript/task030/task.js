bc8be0bcac3b1f8a1ec296d09ad0eb092a10169b
guowenfh <guowen921@live.com>
2016-08-12 23:53:28 +0800

    策略出错，得重新规划写法

--- 2016_spring/graspJavaScript/task030/task.js
+++ 2016_spring/graspJavaScript/task030/task.js
@@ -5,29 +5,29 @@
     /**
      * 表单验证函数
      * @param  {String} text 待验证的输入
+     * @param  {data} data 对象
      * @return {String}      错误码
      */
-    function inputCheck(text) {
+    function inputCheck(text, data) {
         text = gg.trim(text);
-        var result = '';
-        var cont = 0;
+        var map = {
+            name: '名称',
+            password: '密码',
+            password2: '再次输入密码',
+            email: '邮箱',
+            phone: '手机号码',
+        };
         if (text === '') {
-            result = '名字不能为空！';
-        } else {
-            text.split('').forEach(function(item) {
-                if (/[^\x00-\xff]/.test(item)) {
-                    cont += 2;
-                } else {
-                    cont += 1;
-                }
-            });
-            if (cont > 4 && cont <= 16) {
-                result = '名称格式正确';
-            } else {
-                result = '长度为4～16个字符';
-            }
+            return map[data.id] + '不能为空！';
         }
-        return result;
+        if (data.min && text.length < data.min || data.max && text.length > data.max) {
+            return (data.min ? '最少需要' + data.min + '个字符 ' : '') + (data.max ? ' 最多不能超过' + data.max + '个字符' : '');
+        }
+        if(!data.reg || data.reg.test(text)){
+            return data.success;
+        } else {
+            return data.error;
+        }
     }
 
     /**
@@ -59,10 +59,37 @@
             '邮箱应该以类似@gmail.com的格式编写',
             '请正确填写11位手机号码',
         ];
+        var checkArr = [{
+            min: 4,
+            max: 16,
+            success: '名称可用',
+            error: '长度为4-16个字符',
+        }, {
+            reg: /[^\x00-\xff]/,
+            min: 6,
+            success: '密码可用',
+            error: '',
+        }, {
+            reg: /[^\x00-\xff]/,
+            min: 6,
+            success: '密码输入一致',
+            error: '两次输入的密码应该一致',
+        }, {
+            reg: /[^\x00-\xff]/,
+            success: '邮箱可用',
+            error: '邮箱格式错误',
+        }, {
+            reg: /[^\x00-\xff]/,
+            success: '手机格式正确',
+            min:11,
+            error: '手机格式错误',
+        }];
+
         var inputArr = form.querySelectorAll('input');
         inputArr.forEach(function(item, index) {
             gg.addEvent(item, 'focus', function(event) {
                 var tipInfo = this.parentNode.getElementsByClassName('tip');
+                this.className = 'input';
                 if (tipInfo.length === 0) {
                     tipInfo = document.createElement('div');
                     tipInfo.className = 'tip';
@@ -75,8 +102,11 @@
             });
             gg.addEvent(item, 'blur', function(event) {
                 var tipInfo = this.parentNode.getElementsByClassName('tip');
-                console.info(this.value);
-                var tipText = inputCheck(this.value);
+                var checkIndex = checkArr[index];
+                checkIndex.id = this.id;
+                checkIndex.text = foucInfoArr[index];
+                console.info(checkIndex);
+                var tipText = inputCheck(this.value, checkIndex);
                 tipInfo[0].innerHTML = tipText;
                 changeClass([this, tipInfo[0]], false);
             });
