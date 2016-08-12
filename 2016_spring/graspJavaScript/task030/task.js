a2e342fd032e12076c2ac5907aa21775d2113ab7
guowenfh <guowen921@live.com>
2016-08-13 00:17:35 +0800

    完成fouc与bulr部分的的验证逻辑

--- 2016_spring/graspJavaScript/task030/task.js
+++ 2016_spring/graspJavaScript/task030/task.js
@@ -23,7 +23,8 @@
         if (data.min && text.length < data.min || data.max && text.length > data.max) {
             return (data.min ? '最少需要' + data.min + '个字符 ' : '') + (data.max ? ' 最多不能超过' + data.max + '个字符' : '');
         }
-        if(!data.reg || data.reg.test(text)){
+        if (!data.reg || data.reg.test(text)) {
+            data.isPassed = true;
             return data.success;
         } else {
             return data.error;
@@ -62,26 +63,31 @@
         var checkArr = [{
             min: 4,
             max: 16,
+            isPassed: false,
             success: '名称可用',
             error: '长度为4-16个字符',
         }, {
-            reg: /[^\x00-\xff]/,
+            reg: /^(?![0-9]+$)(?![a-zA-Z]+$)[0-9A-Za-z]{6,}$/,
             min: 6,
+            isPassed: false,
             success: '密码可用',
-            error: '',
+            error: '密码应该大于6位且由字母和数字混合组成',
         }, {
-            reg: /[^\x00-\xff]/,
+            reg: /^\w+$/,
             min: 6,
+            isPassed: false,
             success: '密码输入一致',
             error: '两次输入的密码应该一致',
         }, {
-            reg: /[^\x00-\xff]/,
+            reg: /\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*/,
+            isPassed: false,
             success: '邮箱可用',
             error: '邮箱格式错误',
         }, {
-            reg: /[^\x00-\xff]/,
+            reg: /^(0|86|17951)?(13[0-9]|15[012356789]|17[678]|18[0-9]|14[57])[0-9]{8}$/,
+            isPassed: false,
             success: '手机格式正确',
-            min:11,
+            min: 11,
             error: '手机格式错误',
         }];
 
@@ -104,11 +110,13 @@
                 var tipInfo = this.parentNode.getElementsByClassName('tip');
                 var checkIndex = checkArr[index];
                 checkIndex.id = this.id;
+                if (this.id === 'password2') {
+                    checkIndex.reg = new RegExp((document.getElementById('password')).value);
+                }
                 checkIndex.text = foucInfoArr[index];
-                console.info(checkIndex);
                 var tipText = inputCheck(this.value, checkIndex);
                 tipInfo[0].innerHTML = tipText;
-                changeClass([this, tipInfo[0]], false);
+                changeClass([this, tipInfo[0]], checkArr[index].isPassed);
             });
         });
         // 获取标签
