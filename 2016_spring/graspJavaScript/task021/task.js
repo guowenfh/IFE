89f712437ba68ec42d5192c7bf10586d767c8bfb
guowenfh <chenguowen@raycloud.com>
2016-08-21 16:30:30 +0800

    完成不需要参与验证的多选部分

--- 2016_spring/graspJavaScript/task021/task.js
+++ 2016_spring/graspJavaScript/task021/task.js
@@ -1,7 +1,7 @@
-(function () {
+(function() {
     var arr = [];
     // tag输入框
-    function tagInpFun (value, obj) {
+    function tagInpFun(value, obj) {
         if (value !== '' && arr.indexOf(value) === -1) {
             if (arr.length < 10) {
                 arr.push(value);
@@ -14,14 +14,14 @@
     }
     var interest = [];
     // 处理兴趣列表
-    function interestInpFun (value, obj) {
+    function interestInpFun(value, obj) {
         // 将输入分割成数组，并且进行去重操作
-        var vArr = uniqArray(value.split(/\n|\s+|\,|\，|\、|\;|\；\。\./));
+        var vArr = gg.uniqArray(value.split(/[\n|\s+|\,|\，|\、|\;|\；\。\.]+/));
         if ((interest.length + vArr.length) > 0 && (interest.length + vArr.length) < 10) {
             interest = vArr;
         } else {
             interest = interest.concat(vArr);
-            interest = uniqArray(interest);
+            interest = gg.uniqArray(interest);
             if (interest.length > 10) {
                 interest = interest.slice(-10);
             }
@@ -33,10 +33,10 @@
      * @param  {HTMLElement} obj 用于渲染的容器
      * @param  {Array} arr 用于设置的内容
      */
-    function arrToHtml (obj, arr) {
+    function arrToHtml(obj, arr) {
         obj.innerHTML = '';
         if (arr.length !== 0) {
-            arr.forEach(function (ele, index) {
+            arr.forEach(function(ele, index) {
                 if (index < 10) {
                     ele = ele.replace(/\n|\s+|\,|\，|\、|\;|\；\.\。/, '');
                     obj.innerHTML += '<li>' + ele + '</li>';
@@ -49,15 +49,15 @@
      * @param  {HTMLElement} obj [用来事件代理的html元素]
      * @param  {Array} arr [用于渲染html的数组]
      */
-    function removeList (obj, arr) {
-        addEvent(obj, 'mouseover', function (ev) {
+    function removeList(obj, arr) {
+        gg.addEvent(obj, 'mouseover', function(ev) {
             if (ev.target.nodeName.toLowerCase() === 'li') {
                 var target = ev.target;
                 target.innerHTML = '点击删除' + target.innerText;
                 target.style.backgroundColor = '#DF4210';
             }
         });
-        addEvent(obj, 'mouseout', function (ev) {
+        gg.addEvent(obj, 'mouseout', function(ev) {
             if (ev.target.nodeName.toLowerCase() === 'li') {
                 var target = ev.target;
                 var asd = target.innerText;
@@ -67,7 +67,7 @@
             }
         });
         if (arr) {
-            addEvent(obj, 'mousedown', function (ev) {
+            gg.addEvent(obj, 'mousedown', function(ev) {
                 if (ev.target.nodeName.toLowerCase() === 'li') {
                     var target = ev.target;
                     arr.splice(arr.indexOf(target.innerText.substr(4)), 1);
@@ -79,22 +79,22 @@
 
     }
 
-    function init () {
+    function init() {
         var tagInp = document.getElementById('tag-inp'),
             interestInp = document.getElementById('interest-inp'),
             interestBtn = document.getElementById('interest-btn'),
             tagList = document.getElementById('tag-list'),
             interestList = document.getElementById('interest-list');
 
-        addEvent(tagInp, 'keydown', function (ev) {
+        gg.addEvent(tagInp, 'keydown', function(ev) {
             if (ev.keyCode === 13 || ev.keyCode === 186 || ev.keyCode === 188 || ev.keyCode === 32) {
-                tagInpFun(trim(tagInp.value), tagList);
+                tagInpFun(gg.trim(tagInp.value), tagList);
                 this.value = '';
             }
         });
         removeList(tagList, arr);
-        addEvent(interestBtn, 'click', function () {
-            interestInpFun(trim(interestInp.value), interestList);
+        gg.addEvent(interestBtn, 'click', function() {
+            interestInpFun(gg.trim(interestInp.value), interestList);
         });
     }
     init();
--- 2016_spring/graspJavaScript/task032/task.js
+++ 2016_spring/graspJavaScript/task032/task.js
@@ -44,35 +44,90 @@
             });
         };
 
+        this.createInput = function() {
+
+        };
+
+        /**
+         * 创建多选框，单选框，下拉框
+         */
+        this.createMultiple = function(tagName, labelValue) {
+
+            /**
+             * 得到文本域的值，分割成单选框或复选框的值
+             * @param  {String} value 文本域的值
+             * @return {Array}  用于生成列表项的数字
+             */
+            var interestInpFun = function(value) {
+                // 将输入分割成数组，并且进行去重操作
+                value = gg.trim(value);
+                if (value.length < 1) {
+                    alert('请输入内容');
+                    return;
+                }
+                return gg.uniqArray(value.split(/[\n|\s+|\,|\，|\、|\;|\；\。\.]+/));
+            };
+
+            var interestInp = document.getElementById('interest-inp'); // 用于生成多选框的z
+            labelValue = labelValue || '名称';
+            var valueArr = interestInpFun(interestInp.value);
+            console.info(valueArr);
+
+            /**
+             * 用于生成多选框html片段
+             */
+            var html =
+                '<div class="row">' +
+                '    <div class="col-20 inp-label">' + labelValue +
+                '    </div>' +
+                '    <div class="col-80 input-type">';
+            if (tagName === 'select') {
+                html += '<select>';
+                for (var i = 0; i < valueArr.length; i++) {
+                    html += '<option value="' + valueArr[i] + '">' + valueArr[i] + '</option>';
+                }
+                html += '<select>';
+
+            } else {
+                for (var j = 0; j < valueArr.length; j++) {
+                    html +=
+                        '   <label>' +
+                        '       <input value="' + valueArr[j] + '" name="' + labelValue + '" type="' + tagName + '">' + valueArr[j] +
+                        '   </label>';
+                }
+            }
+            html +=
+                '    </div>' +
+                '</div>';
+            return html;
+
+        };
+
+
         /**
          * 点击创建按钮
          */
         this.create = function() {
             var createBtn = document.getElementById('create-btn');
+            var that = this;
             // 表单生成容器
+
             gg.addEvent(createBtn, 'click', function(ev) {
 
                 var tagName = tagGroup.querySelector('input:checked').value; // 标签名字
-                var inp = document.createElement(tagName); // 创建form标签
-
-                if (tagName === 'input') {
-                    var inputType = inptypeGroup.querySelector('input:checked').value; // input类型
-                    inp.type = inputType;
-                }
-                console.info(inputType);
-                console.info(tagName);
+                // var inputType = inptypeGroup.querySelector('input:checked').value; // input类型
                 var labelValue = document.getElementById('label-text').value;
-                var label = document.createElement('label');
-                label.innerText = labelValue;
-
-                // var tip = document.createElement('div');
-                // label.appendChild(inp);
-                // label.appendChild(tip);
-                formContent.appendChild(label);
+                var html;
+                if (tagName === 'radio' || tagName === 'checkbox' || tagName === 'select') {
+                    html = that.createMultiple(tagName, labelValue);
+                } else {
+                    that.createInput();
+                }
+                console.info(tagName);
+                var oDiv = document.createElement('div');
+                oDiv.innerHTML = html;
+                formContent.appendChild(oDiv);
             });
-
-        };
-        this.createInput = function() {
 
         };
         /**
