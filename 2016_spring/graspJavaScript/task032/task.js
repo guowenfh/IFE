8269e8e3b5bc496441c11e24a2aa1f5ea8c49eb6
guowenfh <chenguowen@raycloud.com>
2016-08-21 18:37:57 +0800

    完成整个创建逻辑

--- 2016_spring/graspJavaScript/task032/task.js
+++ 2016_spring/graspJavaScript/task032/task.js
@@ -2,6 +2,55 @@
 (function(window, document) {
 
     /**
+     * 表单验证函数
+     * @param  {String} text 待验证的输入
+     * @param  {data} data 对象
+     * @return {String}      错误码
+     */
+    function inputCheck(text, data) {
+        text = gg.trim(text);
+        var map = {
+            name: '名称',
+            password: '密码',
+            password2: '再次输入密码',
+            email: '邮箱',
+            phone: '手机号码',
+        };
+        if (text === '') {
+            return map[data.id] + '不能为空！';
+        }
+        if (data.min && text.length < data.min || data.max && text.length > data.max) {
+            return (data.min ? '最少需要' + data.min + '个字符 ' : '') + (data.max ? ' 最多不能超过' + data.max + '个字符' : '');
+        }
+        if (!data.reg || data.reg.test(text)) {
+            data.isPassed = true;
+            return data.success;
+        } else {
+            return data.error;
+        }
+    }
+
+    /**
+     * 更改提示样式
+     * @param {Array} eleArr 需要更改样式的元素的数组
+     * @param {String} isSsucce 是否成功
+     */
+    function changeClass(eleArr, isSsucce) {
+        if (isSsucce) {
+            eleArr.forEach(function(item) {
+                // item.className += ' success';
+                gg.addClass(item, 'success');
+            });
+        } else {
+            eleArr.forEach(function(item) {
+                // item.className += ' error';
+                gg.addClass(item, 'error');
+
+            });
+        }
+    }
+
+    /**
      * 创建表单
      */
     function createForm() {
@@ -44,12 +93,44 @@
             });
         };
 
-        this.createInput = function() {
 
+        this.createInput = function(tagName, inputType, labelValue) {
+
+            var label = document.createElement('label');
+            label.innerHTML = '<div class="col-20 inp-label">' + labelValue + '</div>';
+            var inputWrap = document.createElement('div');
+            inputWrap.className = 'col-80';
+            var input;
+            if (tagName === 'textarea') {
+                input = document.createElement('textarea');
+                inputWrap.appendChild(input);
+                label.appendChild(inputWrap);
+                return label;
+            } else {
+                input = document.createElement('input');
+            }
+
+            gg.addClass(input, 'input');
+            var tip = document.createElement('p');
+            tip.innerHTML = '123123';
+
+            tip.className = 'tip';
+            gg.addClass(tip, document.querySelector('.class-select').value);
+
+            gg.addEvent(input, 'blur', function() {
+                changeClass([input, tip], false);
+            });
+            inputWrap.appendChild(input);
+            inputWrap.appendChild(tip);
+            label.appendChild(inputWrap);
+            return label;
         };
 
         /**
          * 创建多选框，单选框，下拉框
+         * @param  {String} tagName    多选类型
+         * @param  {String} labelValue 描述类型
+         * @return {String}            html片段字符串
          */
         this.createMultiple = function(tagName, labelValue) {
 
@@ -77,10 +158,9 @@
              * 用于生成多选框html片段
              */
             var html =
-                '<div class="row">' +
-                '    <div class="col-20 inp-label">' + labelValue +
-                '    </div>' +
-                '    <div class="col-80 input-type">';
+                '<div class="col-20 inp-label">' + labelValue +
+                '</div>' +
+                '<div class="col-80 input-type">';
             if (tagName === 'select') {
                 html += '<select>';
                 for (var i = 0; i < valueArr.length; i++) {
@@ -97,12 +177,9 @@
                 }
             }
             html +=
-                '    </div>' +
                 '</div>';
             return html;
-
         };
-
 
         /**
          * 点击创建按钮
@@ -113,23 +190,21 @@
             // 表单生成容器
 
             gg.addEvent(createBtn, 'click', function(ev) {
-
                 var tagName = tagGroup.querySelector('input:checked').value; // 标签名字
-                // var inputType = inptypeGroup.querySelector('input:checked').value; // input类型
+                var inputType = inptypeGroup.querySelector('input:checked').value; // input类型
                 var labelValue = document.getElementById('label-text').value;
-                var html;
-                if (tagName === 'radio' || tagName === 'checkbox' || tagName === 'select') {
-                    html = that.createMultiple(tagName, labelValue);
-                } else {
-                    that.createInput();
-                }
-                console.info(tagName);
                 var oDiv = document.createElement('div');
-                oDiv.innerHTML = html;
+                gg.addClass(oDiv, 'row');
+                if (tagName === 'radio' || tagName === 'checkbox' || tagName === 'select') {
+                    oDiv.innerHTML = that.createMultiple(tagName, labelValue);
+                } else {
+                    oDiv.appendChild(that.createInput(tagName, inputType, labelValue));
+                }
+
                 formContent.appendChild(oDiv);
             });
-
         };
+
         /**
          * 初始化各种方法
          */
@@ -142,12 +217,13 @@
         this.init();
     }
     createForm();
+
+
 })(window, document);
 
 
 // (function(window, document) {
 //     'use static';
-
 
 //     // {
 //     //     label: '名称',                    // 表单标签
@@ -158,22 +234,6 @@
 //     //     fail: '名称不能为空'               // 验证失败提示
 //     // }
 
-//     /**
-//      * 更改提示样式
-//      * @param {Array} eleArr 需要更改样式的元素的数组
-//      * @param {String} isSsucce 是否成功
-//      */
-//     function changeClass(eleArr, isSsucce) {
-//         if (isSsucce) {
-//             eleArr.forEach(function(item) {
-//                 item.className += ' success';
-//             });
-//         } else {
-//             eleArr.forEach(function(item) {
-//                 item.className += ' error';
-//             });
-//         }
-//     }
 
 
 //     /**
